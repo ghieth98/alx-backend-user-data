d2590bdfbd280905da867ebdbe3374595a811246
hossam ghieth <hossamghieth@gmail.com>
2024-02-14 01:21:15 +0200

    require auth method

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+Module of auth views
+"""
+from typing import List, TypeVar
+
+from flask import request
+
+
+class Auth:
+    """ Auth Class"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Checks if the auth is required"""
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+        if path[-1] != '/':
+            path += '/'
+
+        for i in excluded_paths:
+            if i == path or path.startswith(i.split('*')[0]):
+                return False
+
+        return True
+
+
+def authorization_header(self, request=None) -> str:
+    """ Returns the authorization header"""
+    return None
+
+
+def current_user(self, request=None) -> TypeVar('User'):
+    """ Returns the current user"""
+
+    return None
