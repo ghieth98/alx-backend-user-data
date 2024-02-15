b8541d22d11622e7f358b25a5a8d849a79712626
hossam ghieth <hossamghieth@gmail.com>
2024-02-15 23:35:23 +0200

    Update app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -33,6 +33,9 @@ def before_request() -> None:
                 abort(401)
             if not auth.current_user(request):
                 abort(403)
+            request.current_user = auth.current_user(request)
+        else:
+            return
 
 
 @app.errorhandler(404)
