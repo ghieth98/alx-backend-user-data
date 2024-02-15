783bcd345a22ec73286ad19d732b501cc4f7c571
hossam ghieth <hossamghieth@gmail.com>
2024-02-16 01:07:40 +0200

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python3
+""" Module of Session Auth
 """
-SessionAuth Module
-"""
-from v1.auth.auth import Auth
+from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
-    """
-    SessionAuth Class
-    """
-    pass
+    """ Session Auth"""
