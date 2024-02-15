07d4590c42cdc10d1072f36f58745471f73b61aa
hossam ghieth <hossamghieth@gmail.com>
2024-02-16 01:15:46 +0200

    get user_id for session method

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -23,3 +23,13 @@ class SessionAuth(Auth):
         self.user_id_by_session_id[session_id] = user_id
 
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        Get user id for for the session
+        """
+
+        if session_id is None or type(session_id) is not str:
+            return None
+
+        return self.user_id_by_session_id[session_id]
