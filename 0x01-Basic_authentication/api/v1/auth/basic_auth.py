ce26df078144c1bba108f149ca06b68ca6ae629b
hossam ghieth <hossamghieth@gmail.com>
2024-02-15 23:37:25 +0200

    Update basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -66,10 +66,30 @@ class BasicAuth(Auth):
             return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """Return current user instance"""
+        """ Method that overloads Auth and retrieves the User instance for a
+            request
+        """
+        auth_header = self.authorization_header(request)
 
-        header = self.authorization_header(request)
-        to_base64 = self.extract_base64_authorization_header(header)
-        decode = self.decode_authorization_header(to_base64)
-        extract = self.extract_user_credentials(decode)
-        return self.user_object_from_credentials(*extract)
+        if auth_header is None:
+            return None
+
+        b64_auth_header = self.extract_base64_authorization_header(auth_header)
+
+        if b64_auth_header is None:
+            return None
+
+        decoded_b64_auth_header = self.decode_base64_authorization_header(
+            b64_auth_header)
+
+        if decoded_b64_auth_header is None:
+            return None
+
+        u_email, u_pwd = self.extract_user_credentials(decoded_b64_auth_header)
+
+        if u_email is None or u_pwd is None:
+            return None
+
+        user = self.user_object_from_credentials(u_email, u_pwd)
+
+        return user
