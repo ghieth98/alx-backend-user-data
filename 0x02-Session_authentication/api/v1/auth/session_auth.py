0e5f076b71bea930f7573e10b505e65417c25cf6
hossam ghieth <hossamghieth@gmail.com>
2024-02-16 01:13:27 +0200

    create session method

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python3
 """ Module of Session Auth
 """
+import uuid
+from typing import Dict
+
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
     """ Session Auth"""
+    user_id_by_session_id: Dict[str, str] = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Create a new session id for user
+        """
+
+        if user_id is None or type(user_id) is not str:
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
