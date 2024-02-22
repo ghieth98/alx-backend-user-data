ad9c40b7614f970afc86be65264d9d5a4eb4beed
hossam ghieth <hossamghieth@gmail.com>
2024-02-22 22:23:18 +0200

    added register user method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,10 +5,30 @@ User authentication
 
 from bcrypt import hashpw, gensalt
 
+from db import DB
+from user import User
+
 
 def _hash_password(password: str) -> bytes:
     """
     Hash password with bcrypt
     """
-
     return hashpw(password.encode('utf8'), gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Register a new user
+        """
+        user_email = self._db.find_user_by(email=email)
+        if not user_email:
+            return self._db.add_user(email, _hash_password(password))
+        else:
+            raise ValueError(f"User <user's email> already exists")
