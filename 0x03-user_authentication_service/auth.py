bb6e6101715ca6e28692e5f9f5aa8a924bddd2a6
hossam ghieth <hossamghieth@gmail.com>
2024-02-22 22:55:26 +0200

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,6 +4,7 @@ User authentication
 """
 
 from bcrypt import hashpw, gensalt
+from sqlalchemy.orm.exc import NoResultFound
 
 from db import DB
 from user import User
@@ -27,8 +28,9 @@ class Auth:
         """
         Register a new user
         """
-        user_email = self._db.find_user_by(email=email)
-        if not user_email:
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
         else:
-            raise ValueError(f"User <user's email> already exists")
+            raise ValueError(f'User {email} already exists')
